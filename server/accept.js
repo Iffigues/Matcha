0894c21b396e54a0e212d31a872aeab3bbfefc6d
root <root@vps683807.ovh.net>
2019-08-03 13:21:39 +0200

    fucking routing

--- server/accept.js
+++ server/accept.js
@@ -5,7 +5,7 @@ router.post("/", function (req, res) {
 	let tok = req.body.token;
 	let user = req.body.id;
 	con.connect(function(err) {
-		const f = `SELECT userId, tok FROM verif WHERE userId = ? AND tok = ?`;
+		const f = `DELETE FROM verif WHERE userId = ? AND tok = ?`;
 		con.query(f, [tok, user], function (err, result) {
 			const c = `UPDATE user SET valide=1 WHERE id=?`;
 			if (result && !err)
