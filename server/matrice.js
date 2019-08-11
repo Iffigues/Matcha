1da1793e1ffacfd9395ba5a29aa157dab97e09bc
root <root@vps683807.ovh.net>
2019-08-11 16:25:11 +0200

    profile function pointer

--- server/matrice.js
+++ server/matrice.js
@@ -1,5 +1,11 @@
 class genre {
 	constructor () {
-		this.pref = [];
+		this.flagG = 4194303;
+		this.flagP =  8191;
+		this.genre= ["woman", "man", "agender", "androgynous", "bigender", "genderfluid", "genderqueer", "gendernonconforming", "hijra", "intersex", "cisman", "ciswoman", "nonbinary", "pangender", "transfeminine", "transgender", "transman", "transmasculine", "transexual", "transwoman", "twospirit"];
+		this.pref = ["asexual", "bisexual", "demisexual", "gay", "homoflexible", "heteroflexible", "lesbian", "pansexual", "queer", "questioning", "sapiosexual", "straight", "moreorientations"];
+		this.lien = this.link();
+	}
+	link() {
 	}
 }
