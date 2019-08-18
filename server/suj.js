41b93caa7196fc40e75d6572f613e4dc1d0db23f
root <root@vps683807.ovh.net>
2019-08-18 19:30:42 +0200

    profile function pointer

--- server/suj.js
+++ server/suj.js
@@ -1,3 +1,4 @@
 function suj() {
-	let f = `SELECT *, 3956 * 2 * ASIN(SQRT(POWER(SIN((? - abs(lat)) * pi()/180 / 2),2) + COS(? * pi()/180 ) * COS(abs(lat) *pi()/180) * POWER(SIN((? - lng) *pi()/180 / 2), 2) ))  as distance FROM user_geo WHERE userId = ? having distance < 10000000  ORDER BY distance`;
+	// api = AIzaSyBS2V8xJYNTWhfEFaW7SW0mvOJfoZ1mmI8
+	let f = `SELECT *, 3956 * 2 * ASIN(SQRT(POWER(SIN((? - abs(lat)) * pi()/180 / 2),2) + COS(? * pi()/180 ) * COS(abs(lat) *pi()/180) * POWER(SIN((? - lng) *pi()/180 / 2), 2) ))  as distance FROM user WHERE id  having distance < 10000000  ORDER BY distance AND sexe = ? AND preference = ?`;
 }
