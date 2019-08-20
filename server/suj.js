488c47dd8c6560b8727c3a4eb817f185c662e346
root <root@vps683807.ovh.net>
2019-08-20 20:45:57 +0200

    profile function pointer

--- server/suj.js
+++ server/suj.js
@@ -9,7 +9,13 @@ router.use(middles);
 router.get("/",function (req, res) {
 	let g = `SELECT * FROM user WHERE id = ?`;
 	var decoded = jwtDecode(req.token);
-	let f = `SELECT *, 3956 * 2 * ASIN(SQRT(POWER(SIN((? - abs(lat)) * pi()/180 / 2),2) + COS(? * pi()/180 ) * COS(abs(lat) *pi()/180) * POWER(SIN((? - lng) *pi()/180 / 2), 2) ))  as distance, tag.tag, tag.userId, img.path, (SELECT COUNT(id) FROM tag WHERE tag.tag = tag.tag AND tag.userId = ? ) as match FROM user LEFT JOIN tag as tag ON tag.userId = user.id LEFT JOIN  img as img ON img.userId = user.id WHERE sexe = ? AND preferences = ?   GROUP BY user.username having distance < 100000000`;
+		let f = `SELECT *, 3956 * 2 * ASIN(SQRT(POWER(SIN((? - abs(lat)) * pi()/180 / 2),2) + COS(? * pi()/180 ) * COS(abs(lat) *pi()/180) * POWER(SIN((? - lng) *pi()/180 / 2), 2) ))  as distance, tag.tag, tag.userId, img.path, (SELECT COUNT(id) FROM tag WHERE tag.tag = tag.tag AND tag.userId = ? ) as TT, (SELECT id FROM likes) as lolo 
+	FROM user 
+	LEFT JOIN tag as tag ON tag.userId = user.id 
+	LEFT JOIN  img as img ON img.userId = user.id 
+	INNER JOIN likes as likes ON likes.userOne = user.id
+	WHERE sexe = ? AND preferences = ?   
+	GROUP BY user.username having distance < 100000000`;
 	con.connect(function (err) {
 		con.query(g, [decoded.rr.id], function (err, result) {
 			console.log(err);
