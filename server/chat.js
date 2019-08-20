50571c07feff6374c057c736499e0a105465e547
root <root@vps683807.ovh.net>
2019-08-20 16:09:00 +0200

    profile function pointergp

---
+++ server/chat.js
@@ -0,0 +1,15 @@
+const con = require('./dt.js');
+const express = require('express');
+const  router = express.Router();
+var server = require('http').Server(router);
+const middles = require("./middleware.js");
+var jwtDecode = require('jwt-decode');
+var io = require('socket.io').listen(server);
+
+io.sockets.on('connection', function (socket) {
+	socket.on('switchRoom', function(newroom){
+
+	}
+});
+
+module.exports = router;
--- server/server.js
+++ server/server.js
@@ -15,9 +15,11 @@ const img = require("./img.js");
 const furry = require("./furry.js");
 const suj = require("./suj.js");
 const map = require("./map.js");
-
+const chat = require("./chat.js");
+const http = require('http');
 var cors = require('cors')
 var con =  require("./dt.js");
+
 app.use(cors());
 app.options('*', cors());
 app.get('*', cors());
@@ -38,8 +40,11 @@ app.use(session({
 app.use(function (req, res, next) {;
 	next();
 })
+
+app.use(express.static('public'));
+app.use(express.static(__dirname + '/public'));
+app.use('/public', express.static(__dirname + '/public'));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'build')));
 app.use(bodyParser.urlencoded({ extended: true })); 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -47,10 +52,6 @@ app.use(bodyParser.json({ type: 'application/*+json' }))
 app.use(bodyParser.raw({ type: 'application/vnd.custom-type' })) 
 app.use(bodyParser.text({ type: 'text/html' }))
 app.use(function(req, res, next) {
-	/*res.header("Access-Control-Allow-Origin", "*");
-	res.header('Access-Control-Allow-Credentials', 'true');
-	 res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,PATCH,OPTIONS');
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");*/
 	cors();
 	next();
 });
@@ -64,7 +65,11 @@ app.use('/img', img);
 app.use("/furry", furry);
 app.use("/suj", suj);
 app.use("/map", map);
+app.use("chat", chat);
 app.get('/connected',withAuth, function (req, res) {
 	res.status(200).send(JSON.stringify({code:0, msg:"connecter"}));
 })
-app.listen(process.env.PORT || 8080);
+
+app.listen(process.env.PORT || 8080, function () {
+
+});
