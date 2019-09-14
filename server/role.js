ebc02b90d0c978da468f40ffe4336a24133d2663
Heidy Ben Yahia <heidy.benyahia@gmail.com>
2019-09-14 10:55:44 +0200

    role function dans server marche pas...

--- server/role.js
+++ server/role.js
@@ -3,18 +3,18 @@ const util = require('util');
 const query = util.promisify(con.query).bind(con);
 
 async function role (id) {
-	LET F = `SELECT * FROM USER WHERE ID = ?`;
-	const vv = await query(F,id).thne(rst, err) => {
+	let f = `SELECT * FROM USER WHERE ID = ?`;
+	const vv = await query(F,id).then((rst, err) => {
 		if (rst && rst.length > 0) {
 			let e = rst[0];
 			if (e.birthdate, e.profilephoto, e.city) {
 				const e = await query(`UPDATE user SET role = 'user' WHERE id = ?`,id).then((rd, err) => { 
 					return 1;
-			});
+				});
 			} else {
 				const e = await query(``UPDATE user SET role = 'peuser' WHERE id = ?,id).then((rd, err) => {
 					return 0;
-				})
+				});
 			}
 		}
 	});
